59409a7f0200b3e352ca63068eb7dea42b11686f
ym4199 <ym4199@gmail.com>
2017-06-29 01:31:13 +0900

    html 추가

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Post
+
+
+def index(request):
+    post_list = Post.objects.all()
+    context = {
+        'post_list':post_list,
+    }
+    return render(request, 'blog/post_list.html',context)
