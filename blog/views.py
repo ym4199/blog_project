1dc7a3ccaa3096520992293d8e85ccb26f2fb1bf
ym4199 <ym4199@gmail.com>
2017-06-29 01:57:33 +0900

    detail 구현 추가

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import Post
@@ -7,6 +7,14 @@ from .models import Post
 def index(request):
     post_list = Post.objects.all()
     context = {
-        'post_list':post_list,
+        'post_list': post_list,
     }
-    return render(request, 'blog/post_list.html',context)
+    return render(request, 'blog/post_list.html', context)
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    context = {
+        'post': post,
+    }
+    return render(request, 'blog/post_detail.html', context)
