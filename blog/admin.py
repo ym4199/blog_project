cdccd5276079ab51514063e224b396e9116255e2
ym4199 <ym4199@gmail.com>
2017-06-29 01:00:55 +0900

    blog model 만들고 연결

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'is_public', 'created_date']
+    list_display_links = ['title']
+    list_editable = ['is_public']
